e0696cc1b3eaaacd619572170776bac02581dcae
KV <vinchurewecfund@gmail.com>
2022-06-18 18:25:56 -0700

    uwu senpai notice me

--- app/starbucks_utils.py
+++ app/starbucks_utils.py
@@ -5,7 +5,8 @@ import xmltodict, json
 from functools import lru_cache
 
 
-url = "https://www.starbucks.com/bff/locations?"
+locator_url = "https://www.starbucks.com/bff/locations?"
+menu_url = "https://www.starbucks.com/bff/ordering/menu?"
 #url = "https://www.starbucks.com/bff/locations?lat=40.7127753&lng=-74.0059728&mop=true&place=New%20York%2C%20NY%2C%20USA"
 payload={}
 headers = {
@@ -27,19 +28,32 @@ headers = {
 
 # print(response.text)
 
+def get_menu_by_store(store_id):
+    menu_resp = requests.get(menu_url, params = {"storeNumber" : store_id})
+    return menu_resp.json()
+
 def get_stores_zip_code(zip_code):
     locn = get_lat_lon_from_zip_code(zip_code)
     if len(locn) < 2:
         return locn
     return get_nearby_stores(locn[1-1], locn[2-1])
-    
+
+def get_starbucks_stores_by_product(product, zip_code):
+    stores = get_stores_zip_code(zip_code)
+    if len(stores) == 1:
+        return stores
+    for store in stores:
+       menu =  get_menu_by_store(store["id"])
+       print("new store who dis")
+       print(menu)
 
 def get_nearby_stores(lat, lon):
-    resp = requests.get(url, params={"lat" : lat, "lng" : lon}, headers = headers)
+    resp = requests.get(locator_url, params={"lat" : lat, "lng" : lon}, headers = headers)
     resp_json = resp.json()
     stores = resp_json["stores"]
     return stores
 
 if __name__ == "__main__":
-    print(get_stores_zip_code(95135))
+    #print(get_stores_zip_code(95135))
+    get_starbucks_stores_by_product(-1, 95135)
 
